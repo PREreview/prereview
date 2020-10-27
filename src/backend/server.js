dcd9db8f16a96d46239b651c44dcbe1aa962847f
Josh King <jking@chambana.net>
2020-10-26 21:03:49 -0400

    Fix backend linter errors.

--- src/backend/server.js
+++ src/backend/server.js
@@ -61,16 +61,23 @@ export default function configServer(config) {
 
   // setup API handlers
   const auth = AuthController(userModel, config, authz);
+  // eslint-disable-next-line no-unused-vars
   const commentModel = CommentModel(db);
+  // eslint-disable-next-line no-unused-vars
   const communityModel = CommunityModel(db);
   const fullReviewModel = FullReviewModel(db);
   const prereviews = PrereviewController(fullReviewModel, authz);
+  // eslint-disable-next-line no-unused-vars
   const groupModel = GroupModel(db);
+  // eslint-disable-next-line no-unused-vars
   const personaModel = PersonaModel(db);
   const preprintModel = PreprintModel(db);
   const preprints = PreprintController(preprintModel, authz);
+  // eslint-disable-next-line no-unused-vars
   const rapidReviewModel = RapidReviewModel(db);
+  // eslint-disable-next-line no-unused-vars
   const requestModel = RequestModel(db);
+  // eslint-disable-next-line no-unused-vars
   const tagModel = TagModel(db);
 
   const apiV2Router = compose([
