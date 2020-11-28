c4b42e287a5d32b9f85105613922dcc7c9c979f3
Harum Helmy <harumendhah@gmail.com>
2020-11-28 15:00:07 -0500

    Finished fullReviewDraft controller

--- src/backend/controllers/fullReviewDraft.js
+++ src/backend/controllers/fullReviewDraft.js
@@ -1,12 +1,158 @@
 import router from 'koa-joi-router';
-// import { getLogger } from '../log.js';
+import { getLogger } from '../log.js';
 
-// const log = getLogger('backend:controllers:fullReviewDrafts');
-// const Joi = router.Joi;
+const log = getLogger('backend:controllers:fullReviewDrafts');
+const Joi = router.Joi;
 
-export default function controller() {
-  //drafts, thisUser
+// eslint-disable-next-line no-unused-vars
+export default function controller(draftModel, thisUser) {
   const draftsRouter = router();
+
+  draftsRouter.route({
+    method: 'post',
+    path: '/fullReviewDrafts/',
+    // pre:thisUserthisUser.can('access admin pages'),
+    // validate: {
+    //   body: {
+    //     authors: Joi.array(),
+    //     is_hidden: Joi.boolean(),
+    //     content: Joi.array(),
+    //   },
+    //   type: 'json',
+    //   failure: 400,
+    // },
+    handler: async ctx => {
+      log.debug(`Adding a review draft.`);
+      let draft;
+
+      try {
+        draft = draftModel.create(ctx.request.body);
+        await draftModel.persistAndFlush(draft);
+      } catch (err) {
+        log.error('HTTP 400 Error: ', err);
+        ctx.throw(400, `Failed to parse schema: ${err}`);
+      }
+
+      ctx.body = {
+        status: 201,
+        message: 'created',
+        data: [draft]
+      }
+      ctx.status = 201;
+    },
+  });
+
+  draftsRouter.route({
+    method: 'get',
+    path: '/fullReviewDrafts/',
+    // pre: thisUser.can('access admin pages'),
+    // validate: {},
+    handler: async ctx => {
+      log.debug(`Retrieving review drafts.`);
+      let drafts;
+
+      try {
+        drafts = await draftModel.findAll();
+      } catch (err) {
+        log.error('HTTP 400 Error: ', err);
+        ctx.throw(400, `Failed to parse schema: ${err}`);
+      }
+
+      ctx.body = {
+        status: 200,
+        message: 'ok',
+        data: [drafts]
+      }
+      ctx.status = 200;
+    },
+  });
+
+  draftsRouter.route({
+    method: 'get',
+    path: '/fullReviewDrafts/:id',
+    // pre: thisUser.can('access admin pages'),
+    // validate: {},
+    handler: async ctx => {
+      log.debug(`Retrieving draft with id ${ctx.params.id}.`);
+      let draft;
+
+      try {
+        draft = await draftModel.fidnOne(ctx.params.id);
+        if (!draft) {
+          ctx.throw(404, `Draft with ID ${ctx.params.id} doesn't exist`)
+        }     
+      } catch (err) {
+        log.error('HTTP 400 Error: ', err);
+        ctx.throw(400, `Failed to parse schema: ${err}`);
+      }
+
+      ctx.body = {
+        status: 200,
+        message: 'ok',
+        data: [draft]
+      }
+      ctx.status = 200;
+    },
+  });
+
+  draftsRouter.route({
+    method: 'put',
+    path: '/fullReviewDrafts/:id',
+    // pre:thisUserthisUser.can('access admin pages'),
+    // validate: {
+    //   body: {
+    //     authors: Joi.array(),
+    //     is_hidden: Joi.boolean(),
+    //     content: Joi.array(),
+    //   },
+    //   type: 'json',
+    //   failure: 400,
+    // },
+    handler: async ctx => {
+      log.debug(`Updating review draft ${ctx.params.id}.`);
+      let draft;
+
+      try {
+        draft = await draftModel.findOne(ctx.params.id)
+        if (!draft) {
+          ctx.throw(404, `Draft with ID ${ctx.params.id} doesn't exist`)
+        }
+        draftModel.assign(draft, ctx.request.body);
+        await draftModel.persistAndFlush(draft);
+      } catch (err) {
+        log.error('HTTP 400 Error: ', err);
+        ctx.throw(400, `Failed to parse query: ${err}`);
+      }
+
+      ctx.status = 204;
+    },
+  });
+
+  draftsRouter.route({
+    method: 'delete',
+    path: '/fullReviewDrafts/:id',
+    // pre:thisUserthisUser.can('access admin pages'),
+    handler: async ctx => {
+      log.debug(`Deleting review draft ${ctx.params.id}.`);
+      let draft;
+
+      try {
+        draft = await draftModel.findOne(ctx.params.id)
+        if (!draft) {
+          ctx.throw(404, `Draft with ID ${ctx.params.id} doesn't exist`)
+        }
+        await draftModel.removeAndFlush(draft);
+      } catch (err) {
+        log.error('HTTP 400 Error: ', err);
+        ctx.throw(400, `Failed to parse query: ${err}`);
+      }
+
+      //if deleted
+      ctx.status = 204;
+    },
+  });
+
+  
 
   return draftsRouter;
 }
