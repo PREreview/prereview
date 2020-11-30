d6cb9d71c2dc6b0f72502222272541b72c880089
Harum Helmy <harumendhah@gmail.com>
2020-11-30 15:51:49 -0500

    Enabled request sub-route under preprints

--- src/backend/controllers/request.js
+++ src/backend/controllers/request.js
@@ -9,6 +9,32 @@ const log = getLogger('backend:controllers:tags');
 export default function controller(reqModel, thisUser) {
   const requestRouter = router();
 
+  const getHandler = async ctx => {
+      let requests, pid; // pid = preprint ID
+
+      ctx.params.pid ? pid = ctx.params.pid : null;
+
+      try {
+        if (pid) {
+          log.debug(`Retriving requests made for preprint ${pid}`)
+          requests = await reqModel.find({ preprint: pid });
+        } else {
+          log.debug(`Retrieving all requests.`);
+          requests = await reqModel.findAll();
+        }
+      } catch (err) {
+        log.error('HTTP 400 Error: ', err);
+        ctx.throw(400, `Failed to parse query: ${err}`);
+      }
+
+      ctx.body = {
+        status: 200,
+        message: 'ok',
+        data: requests,
+      };
+      ctx.status = 200;
+  }
+
   requestRouter.route({
     method: 'post',
     path: '/requests',
@@ -42,32 +68,18 @@ export default function controller(reqModel, thisUser) {
     method: 'get',
     path: '/requests',
     // pre: thisUser.can('')
-    //validate: {}
-    handler: async ctx => {
-      log.debug(`Retrieving requests.`);
-      let requests, pid; // pid = preprint ID
-
-      ctx.params.pid ? (pid = ctx.params.pid) : null;
-
-      try {
-        if (pid) {
-          requests = await reqModel.find({ preprint: pid });
-        } else {
-          requests = await reqModel.findAll();
-        }
-      } catch (err) {
-        log.error('HTTP 400 Error: ', err);
-        ctx.throw(400, `Failed to parse query: ${err}`);
-      }
-
-      ctx.body = {
-        status: 200,
-        message: 'ok',
-        data: requests,
-      };
-      ctx.status = 200;
-    },
+    // validate: {}
+    handler: async ctx => getHandler(ctx),
   });
+
+   requestRouter.route({
+    method: 'get',
+    path: '/preprints/:pid/requests',
+    // pre: thisUser.can('')
+    //validate: {}
+    handler: async ctx => getHandler(ctx),
+  });
+
 
   requestRouter.route({
     method: 'get',
