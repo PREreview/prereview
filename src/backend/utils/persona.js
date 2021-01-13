ac49a3a55d17ec16502f3ab35771b585e57a4bbe
Harum Helmy <harumendhah@gmail.com>
2021-01-13 10:18:36 -0600

    Updated getActivePersona function

--- src/backend/utils/persona.js
+++ src/backend/utils/persona.js
@@ -2,7 +2,5 @@
 // the user object received here is ctx.state.user
 
 export default async function getActivePersona(user) {
-  const personas = await user.personas.loadItems();
-  const active = personas.filter(persona => persona.isActive === true)[0];
-  return active;
+  return user.defaultPersona
 }
