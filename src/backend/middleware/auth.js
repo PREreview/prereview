ebb4d3c68427ef701d2ea10760ad040bc3fce50b
Harum Helmy <harumendhah@gmail.com>
2021-01-20 17:34:36 -0500

    Removed errant console logs

--- src/backend/middleware/auth.js
+++ src/backend/middleware/auth.js
@@ -39,11 +39,8 @@ const authWrapper = groups => {
   });
 
   roles.use('access admin pages', async ctx => {
-    log.debug('ctx.isAuthenticated', ctx.isAuthenticated());
     log.debug('Checking if user can access admin pages.');
     if (!ctx.isAuthenticated()) return false;
-
-    log.debug('ctx.state.user', ctx.state.user);
 
     return roles.isMemberOf('admins', ctx.state.user.orcid);
   });
