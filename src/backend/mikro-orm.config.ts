9d5f4f9059c8430566688c681cb5bd4dc419f86f
Josh King <jking@chambana.net>
2021-01-24 17:11:07 -0500

    Fix SQL query debugging.

--- src/backend/mikro-orm.config.ts
+++ src/backend/mikro-orm.config.ts
@@ -45,9 +45,9 @@ const options: Options = {
     Work,
   ],
   type: dbType,
-  debug: true,
-  clientUrl: `${dbType}://${authString}${config.dbHost}${portString}/${config.dbName
-    }`,
+  debug: config.logLevel === 'trace',
+  clientUrl: `
+  ${dbType}://${authString}${config.dbHost}${portString}/${config.dbName}`,
   cache: {
     pretty: true,
     options: {
