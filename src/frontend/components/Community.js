8b643fa7e56b678236376e28dcd2bc7cff76e4bd
rudietuesdays <rae@throneless.tech>
2021-01-29 16:15:31 -0500

    wip styles on community page

--- src/frontend/components/Community.js
+++ src/frontend/components/Community.js
@@ -1,8 +1,8 @@
 // base imports
 import React, { Suspense, useContext, useState } from 'react';
+import PropTypes from 'prop-types';
 import { useHistory, useLocation, useParams } from 'react-router-dom';
 import { Helmet } from 'react-helmet-async';
-import PropTypes from 'prop-types';
 import MuiSearchBar from 'material-ui-search-bar';
 
 // contexts
@@ -12,6 +12,7 @@ import UserProvider from '../contexts/user-context';
 import { useGetCommunity, useGetPreprints } from '../hooks/api-hooks.tsx';
 
 // components
+import AddButton from './add-button';
 import HeaderBar from './header-bar';
 import Loading from './loading';
 import NotFound from './not-found';
@@ -37,18 +38,12 @@ import Select from '@material-ui/core/Select';
 import Chip from '@material-ui/core/Chip';
 
 const useStyles = makeStyles(theme => ({
-  sidebarBox: {
-    maxWidth: theme.breakpoints.values['md'],
+  avatar: {
+    width: theme.spacing(10),
+    height: theme.spacing(10),
     marginLeft: 'auto',
     marginRight: 'auto',
-    paddingTop: theme.spacing(12),
-    paddingBottom: theme.spacing(8),
-    textAlign: 'center',
-    [theme.breakpoints.up('lg')]: {
-      paddingTop: theme.spacing(16),
-      paddingBottom: theme.spacing(16),
-      textAlign: 'left',
-    },
+    marginBottom: theme.spacing(2),
   },
   banner: {
     maxWidth: theme.breakpoints.values['md'],
@@ -64,21 +59,39 @@ const useStyles = makeStyles(theme => ({
     },
   },
   contentMain: {
+    marginTop: '2rem',
+    padding: theme.spacing(2),
+  },
+  paper: {
+    borderRadius: 20,
+    boxShadow: '1px 1px 5px 3px #DADADA',
     maxWidth: theme.breakpoints.values['md'],
     marginLeft: 'auto',
     marginRight: 'auto',
-    textAlign: 'center',
-    padding: theme.spacing(2),
     [theme.breakpoints.up('lg')]: {
       textAlign: 'left',
     },
   },
-  avatar: {
-    width: theme.spacing(10),
-    height: theme.spacing(10),
+  search: {
+    borderRadius: 24,
+    paddingLeft: 10,
+    paddingRight: 10,
+  },
+  select: {
+    minWidth: 200,
+  },
+  sidebarBox: {
+    maxWidth: theme.breakpoints.values['md'],
     marginLeft: 'auto',
     marginRight: 'auto',
-    marginBottom: theme.spacing(2),
+    paddingTop: theme.spacing(12),
+    paddingBottom: theme.spacing(8),
+    textAlign: 'center',
+    [theme.breakpoints.up('lg')]: {
+      paddingTop: theme.spacing(16),
+      paddingBottom: theme.spacing(16),
+      textAlign: 'left',
+    },
   },
 }));
 
@@ -158,7 +171,11 @@ export default function Community() {
           />
           <Grid container spacing={2}>
             <Grid item xs={12} md={8}>
-              <CommunityContent thisUser={user} community={community} params={params} />
+              <CommunityContent
+                thisUser={user}
+                community={community}
+                params={params}
+              />
             </Grid>
             <Grid item xs={12} md={4}>
               {community.members &&
@@ -201,7 +218,7 @@ function CommunityHeader({ name, description }) {
               <Typography variant="h3" component="h2" gutterBottom={true}>
                 {name}
               </Typography>
-              <Typography variant="h5" color="textSecondary" paragraph={true}>
+              <Typography variant="h5" color="textSecondary" paragraph>
                 {description}
               </Typography>
             </Box>
@@ -297,7 +314,7 @@ function CommunityContent({ thisUser, community, params }) {
     return <div>An error occurred: {error}</div>;
   } else {
     return (
-      <Paper className={classes.contentMain}>
+      <Box m={2}>
         <CommunitySearch
           defaultValue={search}
           isFetching={loadingPreprints}
@@ -330,85 +347,124 @@ function CommunityContent({ thisUser, community, params }) {
             }}
           />
         )}
-        <FormControl className={classes.formControl}>
-          <InputLabel id="community-tags">Tags</InputLabel>
-          <Select
-            labelId="community-tags"
-            id="community-tags"
-            multiple
-            value={selectedTags}
-            onChange={ev => setSelectedTags(ev.target.value)}
-            input={<Input id="community-tags-select-multiple" />}
-            renderValue={selected => (
-              <div className={classes.chips}>
-                {selected.map(value => (
-                  <Chip key={value} label={value} className={classes.chip} />
-                ))}
-              </div>
-            )}
-          >
-            {community.tags.map(tag => (
-              <MenuItem key={tag.uuid} value={tag.name}>
-                {tag.name}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-        {preprints && preprints.totalCount > 0 && !loadingPreprints && (
-          <SortOptions
-            sort={params.get('sort') || ''}
-            order={params.get('asc') === 'true' ? 'asc' : 'desc'}
-            onMouseEnterSortOption={sortOption => {
-              setHoveredSortOption(sortOption);
-            }}
-            onMouseLeaveSortOption={() => {
-              setHoveredSortOption(null);
-            }}
-            onChange={(sortOption, sortOrder) => {
-              params.set('asc', sortOrder === 'asc');
-              params.set('sort', sortOption);
-              history.push({
-                pathname: location.pathame,
-                search: params.toString(),
-              });
-            }}
-          />
-        )}
-        {preprints && preprints.totalCount === 0 && !loadingPreprints ? (
-          <div>No preprints have been added to this community.</div>
-        ) : (
-          preprints &&
-          preprints.data.map(row => (
-            <Grid item key={row.id}>
-              <PreprintCard
-                isNew={false}
-                user={thisUser}
-                preprint={row}
-                onNewRequest={handleNewRequest}
-                onNew={handleNew}
-                onNewReview={handleNewReview}
-                hoveredSortOption={hoveredSortOption}
-                sortOption={params.get('asc') === 'true'}
-              />
+        <Paper className={`${classes.contentMain} ${classes.paper}`}>
+          <Box mb={4}>
+            <Grid container justify="space-between">
+              <Grid item xs={12} sm={6}>
+                <Typography
+                  component="h3"
+                  variant="h6"
+                  align="left"
+                  gutterBottom
+                >
+                  Preprints with reviews or requests for reviews
+                </Typography>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <Box align="right">
+                  <AddButton
+                    onClick={() => {
+                      if (thisUser) {
+                        history.push('/new');
+                      } else {
+                        setLoginModalOpenNext('/new');
+                      }
+                    }}
+                    disabled={location.pathname === '/new'}
+                  />
+                </Box>
+              </Grid>
             </Grid>
-          ))
-        )}
-        {preprints && preprints.totalCount > params.get('limit') && (
-          <Grid item>
-            <Pagination
-              count={Math.ceil(preprints.totalCount / params.get('limit'))}
-              page={parseInt('' + params.get('page'))}
-              onChange={(ev, page) => {
-                params.set('page', page);
-                history.push({
-                  pathname: location.pathname,
-                  search: params.toString(),
-                });
-              }}
-            />
+          </Box>
+          <Grid className={classes.formControl} container spacing={2}>
+            <Grid item xs={12} sm={6} md={3}>
+              <InputLabel id="community-tags-label">Tags</InputLabel>
+              <Select
+                labelId="community-tags-label"
+                id="community-tags"
+                className={classes.select}
+                multiple
+                value={selectedTags}
+                onChange={ev => setSelectedTags(ev.target.value)}
+                input={<Input id="community-tags-select-multiple" />}
+                renderValue={selected => (
+                  <div className={classes.chips}>
+                    {selected.map(value => (
+                      <Chip
+                        key={value}
+                        label={value}
+                        className={classes.chip}
+                      />
+                    ))}
+                  </div>
+                )}
+              >
+                {community.tags.map(tag => (
+                  <MenuItem key={tag.uuid} value={tag.name}>
+                    {tag.name}
+                  </MenuItem>
+                ))}
+              </Select>
+            </Grid>
           </Grid>
-        )}
-      </Paper>
+          <Box mt={4}>
+            {preprints && preprints.totalCount > 0 && !loadingPreprints && (
+              <SortOptions
+                sort={params.get('sort') || ''}
+                order={params.get('asc') === 'true' ? 'asc' : 'desc'}
+                onMouseEnterSortOption={sortOption => {
+                  setHoveredSortOption(sortOption);
+                }}
+                onMouseLeaveSortOption={() => {
+                  setHoveredSortOption(null);
+                }}
+                onChange={(sortOption, sortOrder) => {
+                  params.set('asc', sortOrder === 'asc');
+                  params.set('sort', sortOption);
+                  history.push({
+                    pathname: location.pathame,
+                    search: params.toString(),
+                  });
+                }}
+              />
+            )}
+            {preprints && preprints.totalCount === 0 && !loadingPreprints ? (
+              <div>No preprints have been added to this community.</div>
+            ) : (
+              preprints &&
+              preprints.data.map(row => (
+                <Grid item key={row.id}>
+                  <PreprintCard
+                    isNew={false}
+                    user={thisUser}
+                    preprint={row}
+                    onNewRequest={handleNewRequest}
+                    onNew={handleNew}
+                    onNewReview={handleNewReview}
+                    hoveredSortOption={hoveredSortOption}
+                    sortOption={params.get('asc') === 'true'}
+                  />
+                </Grid>
+              ))
+            )}
+            {preprints && preprints.totalCount > params.get('limit') && (
+              <Grid item>
+                <Pagination
+                  count={Math.ceil(preprints.totalCount / params.get('limit'))}
+                  page={parseInt('' + params.get('page'))}
+                  onChange={(ev, page) => {
+                    params.set('page', page);
+                    history.push({
+                      pathname: location.pathname,
+                      search: params.toString(),
+                    });
+                  }}
+                />
+              </Grid>
+            )}
+          </Box>
+        </Paper>
+      </Box>
     );
   }
 }
@@ -420,18 +476,20 @@ function CommunitySearch({
   onCancelSearch,
   onRequestSearch,
 }) {
+  const classes = useStyles();
+
   return (
-    <div>
+    <Box mb={1}>
       <MuiSearchBar
         value={defaultValue}
         onChange={value => onChange(value)}
         onCancelSearch={onCancelSearch}
         onRequestSearch={onRequestSearch}
-        className="search-bar__search-box__input"
+        className={classes.search}
         placeholder={'Search by DOI, arXiv ID, title, author, or abstract'}
         disabled={isFetching}
       />
-    </div>
+    </Box>
   );
 }
 
