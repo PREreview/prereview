95a8d96c37d458e1fc2b94219720d6df9218e709
rudietuesdays <rae@throneless.tech>
2021-02-11 12:39:22 -0500

    moar linting fixes

--- src/frontend/components/review-stepper.js
+++ src/frontend/components/review-stepper.js
@@ -234,8 +234,8 @@ function QontoStepIcon(props) {
       {completed ? (
         <Check className={classes.completed} />
       ) : (
-          <div className={classes.circle} />
-        )}
+        <div className={classes.circle} />
+      )}
     </div>
   );
 }
@@ -370,7 +370,9 @@ export default function ReviewStepper({
       if (cid) {
         putLongReview({
           contents: content,
-          authors: review ? review.authors.map(author => ({ uuid: author.uuid })) : null,
+          authors: review
+            ? review.authors.map(author => ({ uuid: author.uuid }))
+            : null,
         })
           .then(() => alert('Draft updated successfully.'))
           .catch(err => alert(`An error occurred: ${err.message}`));
@@ -378,7 +380,9 @@ export default function ReviewStepper({
         postLongReview({
           preprint: preprint.uuid,
           contents: content,
-          authors: review ? review.authors.map(author => ({ uuid: author.uuid })) : null,
+          authors: review
+            ? review.authors.map(author => ({ uuid: author.uuid }))
+            : null,
         })
           .then(response => {
             alert('Draft updated successfully.');
@@ -406,7 +410,9 @@ export default function ReviewStepper({
           preprint: preprint.uuid,
           contents: content,
           isPublished: true,
-          authors: review ? review.authors.map(author => ({ uuid: author.uuid })) : null,
+          authors: review
+            ? review.authors.map(author => ({ uuid: author.uuid }))
+            : null,
         })
           .then(() => {
             setActiveStep(prevActiveStep => prevActiveStep + 2);
@@ -606,8 +612,15 @@ export default function ReviewStepper({
                 spacing={2}
               >
                 <Grid item xs={12} sm={6}>
-                  <AddAuthors reviewId={cid} authors={review ? review.authors : null} />
-                  <AddAuthors isMentor={true} reviewId={cid} authors={review ? review.mentors : null} />
+                  <AddAuthors
+                    reviewId={cid}
+                    authors={review ? review.authors : null}
+                  />
+                  <AddAuthors
+                    isMentor={true}
+                    reviewId={cid}
+                    authors={review ? review.mentors : null}
+                  />
                 </Grid>
                 <Grid item xs={12} sm={6}>
                   <Box textAlign="right" mr={2}>
@@ -639,14 +652,14 @@ export default function ReviewStepper({
                         >
                           {templates
                             ? templates.data.map(template => (
-                              <MenuItem
-                                key={template.uuid}
-                                value={template.contents}
-                                className={classes.template}
-                              >
-                                {template.title}
-                              </MenuItem>
-                            ))
+                                <MenuItem
+                                  key={template.uuid}
+                                  value={template.contents}
+                                  className={classes.template}
+                                >
+                                  {template.title}
+                                </MenuItem>
+                              ))
                             : null}
                         </Select>
                         {template ? (
@@ -805,39 +818,39 @@ export default function ReviewStepper({
               Congratulations! You have successfully submitted your PREreview.
             </Box>
           ) : (
-              <div>
-                {getStepContent(activeStep)}
-                <Box textAlign="right">
-                  {activeStep === 0 ? (
-                    <>
-                      {hasRapidReviewed && (
-                        <Button
-                          variant="outlined"
-                          color="primary"
-                          onClick={() => handleComplete(2)}
-                          className={classes.button}
-                        >
-                          No
-                        </Button>
-                      )}
+            <div>
+              {getStepContent(activeStep)}
+              <Box textAlign="right">
+                {activeStep === 0 ? (
+                  <>
+                    {hasRapidReviewed && (
+                      <Button
+                        variant="outlined"
+                        color="primary"
+                        onClick={() => handleComplete(2)}
+                        className={classes.button}
+                      >
+                        No
+                      </Button>
+                    )}
 
-                      {!hasLongReviewed ? (
-                        <Button
-                          variant="contained"
-                          color="primary"
-                          onClick={handleNext}
-                          className={classes.button}
-                        >
-                          {!hasRapidReviewed && activeStep === 0
-                            ? 'Submit'
-                            : 'Yes'}
-                        </Button>
-                      ) : null}
-                    </>
-                  ) : null}
-                </Box>
-              </div>
-            )}
+                    {!hasLongReviewed ? (
+                      <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={handleNext}
+                        className={classes.button}
+                      >
+                        {!hasRapidReviewed && activeStep === 0
+                          ? 'Submit'
+                          : 'Yes'}
+                      </Button>
+                    ) : null}
+                  </>
+                ) : null}
+              </Box>
+            </div>
+          )}
         </div>
       </div>
     </ThemeProvider>
