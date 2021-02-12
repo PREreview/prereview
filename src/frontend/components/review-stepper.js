bdcbfcd3e443e1cf5f67e9607da78aa58b6f1590
Harum Helmy <harumendhah@gmail.com>
2021-02-12 16:35:54 -0500

    Add conditional to ensure app doesnt error out without multiple authors

--- src/frontend/components/review-stepper.js
+++ src/frontend/components/review-stepper.js
@@ -368,7 +368,7 @@ export default function ReviewStepper({
       if (cid) {
         putLongReview({
           contents: content,
-          authors: review ? review.authors.map(author => ({ uuid: author.uuid })) : null,
+          authors: review && review.authors ? review.authors.map(author => ({ uuid: author.uuid })) : null,
         })
           .then(() => alert('Draft updated successfully.'))
           .catch(err => alert(`An error occurred: ${err.message}`));
@@ -376,7 +376,7 @@ export default function ReviewStepper({
         postLongReview({
           preprint: preprint.uuid,
           contents: content,
-          authors: review ? review.authors.map(author => ({ uuid: author.uuid })) : null,
+          authors: review && review.authors ? review.authors.map(author => ({ uuid: author.uuid })) : null,
         })
           .then(response => {
             alert('Draft updated successfully.');
@@ -404,7 +404,7 @@ export default function ReviewStepper({
           preprint: preprint.uuid,
           contents: content,
           isPublished: true,
-          authors: review ? review.authors.map(author => ({ uuid: author.uuid })) : null,
+          authors: review && review.authors ? review.authors.map(author => ({ uuid: author.uuid })) : null,
         })
           .then(() => {
             setActiveStep(prevActiveStep => prevActiveStep + 2);
