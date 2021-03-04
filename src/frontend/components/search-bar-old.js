2f59f0c24f9238fa6dcb2d11ddf05bd61d0fc278
rudietuesdays <rae@throneless.tech>
2021-03-04 15:46:13 -0500

    move search bar to MUI component

--- src/frontend/components/search-bar-old.js
+++ src/frontend/components/search-bar-old.js
@@ -1,20 +1,6 @@
-// base imports
 import React from 'react';
 import PropTypes from 'prop-types';
-
-// material ui imports
-import { makeStyles } from '@material-ui/core/styles';
-import Box from '@material-ui/core/Box';
 import MuiSearchBar from 'material-ui-search-bar';
-
-const useStyles = makeStyles(theme => ({
-  searchbar: {
-
-  },
-  searchbarInner: {
-    borderRadius: '50%',
-  },
-}));
 
 export default function SearchBar({
   isFetching,
@@ -23,11 +9,9 @@ export default function SearchBar({
   onCancelSearch,
   onRequestSearch,
 }) {
-  const classes = useStyles();
-
   return (
-    <Box className={classes.searchbar}>
-      <Box className={classes.searchbarInner}>
+    <div className="search-bar">
+      <div className="search-bar__search-box">
         <MuiSearchBar
           value={defaultValue}
           onChange={value => onChange(value)}
@@ -37,8 +21,8 @@ export default function SearchBar({
           placeholder="Enter search terms here"
           disabled={isFetching}
         />
-      </Box>
-    </Box>
+      </div>
+    </div>
   );
 }
 
