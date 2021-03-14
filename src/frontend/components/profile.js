738dd4a8730dd3e827e18f81b40e7270b4df865f
Harum Helmy <harumendhah@gmail.com>
2021-03-14 19:07:54 -0400

    WIP profile page, implementing dropdown

--- src/frontend/components/profile.js
+++ src/frontend/components/profile.js
@@ -8,7 +8,7 @@ import { format } from 'date-fns';
 import UserProvider from '../contexts/user-context';
 
 // hooks
-import { useGetPersona } from '../hooks/api-hooks.tsx';
+import { useGetPersona, usePutUser } from '../hooks/api-hooks.tsx';
 
 // Material UI components
 import { makeStyles, withStyles } from '@material-ui/core/styles';
@@ -16,12 +16,14 @@ import Avatar from '@material-ui/core/Avatar';
 import Box from '@material-ui/core/Box';
 import Chip from '@material-ui/core/Chip';
 import Container from '@material-ui/core/Container';
+import FormHelperText from '@material-ui/core/FormHelperText';
 import IconButton from '@material-ui/core/IconButton';
 import Link from '@material-ui/core/Link';
+import MenuItem from '@material-ui/core/MenuItem';
 import Modal from '@material-ui/core/Modal';
-import Switch from '@material-ui/core/Switch';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
+import Select from '@material-ui/core/Select';
 
 // components
 import HeaderBar from './header-bar';
@@ -35,64 +37,17 @@ import XLink from './xlink';
 // constants
 import { ORG } from '../constants';
 
-const PersonaSwitch = withStyles(theme => ({
-  root: {
-    width: 70,
-    height: 30,
-    padding: 2,
-    margin: theme.spacing(1),
-  },
-  switchBase: {
-    padding: 1,
-    '&$checked': {
-      transform: 'translateX(44px)',
-      color: theme.palette.common.white,
-      '& + $track': {
-        backgroundColor: '#52d869',
-        opacity: 1,
-        border: 'none',
-      },
-    },
-    '&$focusVisible $thumb': {
-      color: '#52d869',
-      border: '6px solid #fff',
-    },
-  },
-  thumb: {
-    width: 27,
-    height: 27,
-  },
-  track: {
-    borderRadius: 30 / 2,
-    border: `1px solid ${theme.palette.grey[400]}`,
-    backgroundColor: theme.palette.grey[50],
-    opacity: 1,
-    transition: theme.transitions.create(['background-color', 'border']),
-  },
-  checked: {},
-  focusVisible: {},
-}))(({ classes, ...props }) => {
-  console.log("switch persona")
-  return (
-    <Switch
-      focusVisibleClassName={classes.focusVisible}
-      disableRipple
-      classes={{
-        root: classes.root,
-        switchBase: classes.switchBase,
-        thumb: classes.thumb,
-        track: classes.track,
-        checked: classes.checked,
-      }}
-      {...props}
-    />
-  );
-});
-
 const useStyles = makeStyles(theme => ({
   avatar: {
     height: 220,
     width: 220,
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+  selectEmpty: {
+    marginTop: theme.spacing(2),
   },
 }));
 
@@ -102,23 +57,49 @@ export default function Profile() {
   // const [editAvatar, setEditAvatar] = useState(false);
   const { id } = useParams();
   const history = useHistory();
-  const ownProfile = !thisUser ? false : thisUser.personas.some(persona => persona.uuid === id); // returns true if the profile page belongs to the logged in user
-  const anonPersona = !thisUser ? null : thisUser.personas.filter(persona => persona.isAnonymous)[0]
-  const publicPersona = !thisUser ? null : thisUser.personas.filter(persona => !persona.isAnonymous)[0]
-  const [persona, setPersona] = useState(thisUser ? thisUser.defaultPersona : {})
+  const ownProfile = thisUser
+    ? thisUser.personas.some(persona => persona.uuid === id)
+    : false; // returns true if the profile page belongs to the logged in user
 
   const { data: persona, loading, error } = useGetPersona({
     id: id,
     resolve: persona => persona.data[0],
   });
 
-  const [checked, setChecked] = useState(true)
+  const { mutate: updateUser, loadingUser } = usePutUser({
+    id: thisUser.uuid,
+  });
 
-  const handleSwitch = () => {
-    setChecked(!checked)
-    setPersona(checked ? anonPersona : publicPersona);
-    setUser({...thisUser, defaultPersona: checked ? anonPersona : publicPersona})
-  }
+  const personas = !thisUser ? [] : thisUser.personas;
+
+  const [selectedPersona, setSelectedPersona] = useState(
+    thisUser ? thisUser.defaultPersona : {},
+  );
+
+  const handleChange = e => {
+    updateUser({ defaultPersona: e.target.value.id })
+      .then(() => {
+        alert(`You've successfully updated your active persona.`);
+        setSelectedPersona(e.target.value);
+      })
+      .catch(err => alert(`An error occurred: ${err.message}`));
+  };
+
+  useEffect(() => {
+    console.log('useEffect firing');
+    setUser({ ...thisUser, defaultPersona: selectedPersona });
+  }, [selectedPersona]);
+
+  console.log(
+    'console logging to see whether the defaultPersona changes',
+    thisUser.defaultPersona.name,
+  );
+
+  persona
+    ? console.log('persona!!!!!!!', persona, persona.name)
+    : console.log('no persona');
+  console.log('selectedPersona,', selectedPersona, selectedPersona.name);
+  personas.map(persona => console.log('this persona!', persona.name));
 
   if (!persona || loading) {
     return <Loading />;
@@ -155,19 +136,30 @@ export default function Profile() {
                     >
                       <Grid item xs={4}>
                         <Typography component="div" variant="body1">
-                          Public
+                          Active persona:
                         </Typography>
                       </Grid>
-                      <Grid item xs={4}>
-                        <PersonaSwitch
-                          checked={checked}
-                          onChange={handleSwitch}
-                        />
-                      </Grid>
-                      <Grid item xs={4}>
-                        <Typography component="div" variant="body1">
-                          Anonymous
-                        </Typography>
+                      <Grid item md={10}>
+                        <Select
+                          value={selectedPersona}
+                          onChange={handleChange}
+                          className={classes.selectEmpty}
+                          renderValue={selected => selected.name}
+                        >
+                          {personas
+                            ? personas.map(p => {
+                                return (
+                                  <MenuItem key={p.uuid} value={p}>
+                                    {p.name}
+                                  </MenuItem>
+                                );
+                              })
+                            : null}
+                        </Select>
+                        <FormHelperText>
+                          Choose the profile persona you wish to use as default
+                          in your activity.
+                        </FormHelperText>
                       </Grid>
                     </Grid>
                     <Grid item xs={12} md={6}>
@@ -184,23 +176,37 @@ export default function Profile() {
               <Container>
                 <Grid container justify="space-between" alignItems="flex-start">
                   <Grid item>
+                    <Box>
+                      <Typography component="div" variant="h6" gutterBottom>
+                        {persona.name}
+                      </Typography>
+                    </Box>
                     {!persona.isAnonymous && (
                       <Box>
-                        <Typography component="div" variant="h6" gutterBottom>
-                          {persona.name}
-                        </Typography>
-                        <Typography component="div" variant="body1" gutterBottom>
+                        <Typography
+                          component="div"
+                          variant="body1"
+                          gutterBottom
+                        >
                           <Link
                             href={`https://orcid.org/${persona.identity.orcid}`}
                           >
                             ORCiD: {persona.identity.orcid}
                           </Link>
                         </Typography>
-                        <Typography component="div" variant="body1" gutterBottom>
+                        <Typography
+                          component="div"
+                          variant="body1"
+                          gutterBottom
+                        >
                           <b>Email address: </b>
                           {persona.email ? persona.email : `None provided`}
                         </Typography>
-                        <Typography component="div" variant="body1" gutterBottom>
+                        <Typography
+                          component="div"
+                          variant="body1"
+                          gutterBottom
+                        >
                           <b>Badges: </b>
                           {persona.badges &&
                             persona.badges.length > 0 &&
@@ -213,10 +219,18 @@ export default function Profile() {
                               />
                             ))}
                         </Typography>
-                        <Typography component="div" variant="body1" gutterBottom>
+                        <Typography
+                          component="div"
+                          variant="body1"
+                          gutterBottom
+                        >
                           <b>Area(s) of expertise: </b>
                         </Typography>
-                        <Typography component="div" variant="body1" gutterBottom>
+                        <Typography
+                          component="div"
+                          variant="body1"
+                          gutterBottom
+                        >
                           Community member since{' '}
                           {format(new Date(persona.createdAt), 'MMM. d, yyyy')}
                         </Typography>
@@ -226,7 +240,10 @@ export default function Profile() {
                   <Grid item>
                     {ownProfile ? (
                       <IconButton href="/settings">
-                        <Avatar src={persona.avatar} className={classes.avatar} />
+                        <Avatar
+                          src={persona.avatar}
+                          className={classes.avatar}
+                        />
                       </IconButton>
                     ) : (
                       <Avatar src={persona.avatar} className={classes.avatar} />
