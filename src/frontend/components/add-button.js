c610ea7d3d8b0eadc3828fce1bc141c5e50e75e9
rudietuesdays <rae@throneless.tech>
2021-03-26 16:48:36 -0400

    port add button to MUI

--- src/frontend/components/add-button.js
+++ src/frontend/components/add-button.js
@@ -1,15 +1,58 @@
+// base imports
 import React from 'react';
-import Button from './button';
+
+// Material UI imports
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+
+// icons
 import icon from '../svgs/add_prereview_icon.svg';
 
+const useStyles = makeStyles(theme => ({
+  border: {
+    borderRight: `2px solid ${theme.palette.primary.contrastText}`,
+    paddingRight: 10,
+  },
+  button: {
+    backgroundColor: theme.palette.primary.main,
+    borderRadius: 50,
+    color: theme.palette.primary.contrastText,
+    textTransform: 'none',
+    width: 300,
+    '&:hover': {
+      backgroundColor: theme.palette.primary.dark,
+    },
+  },
+  icon: {
+    backgroundColor: theme.palette.primary.contrastText,
+    borderRadius: '50%',
+    height: 20,
+    marginRight: 10,
+    padding: 2,
+    width: 20,
+  },
+  text: {
+    fontSize: '1.2rem',
+    marginRight: 10,
+  },
+}));
+
 export default function AddButton({ ...buttonProps }) {
+  const classes = useStyles();
   return (
-    <Button className="add-button" {...buttonProps} pill={true} primary={true}>
-      <div className="add-button__icon-container">
-        <img src={icon} aria-hidden="true" alt="" />
-      </div>
-      <div className="add-button__left">Add Review</div>
-      <div className="add-button__right">Add Request</div>
+    <Button className={classes.button} primary="true" {...buttonProps}>
+      <img src={icon} aria-hidden="true" alt="" className={classes.icon} />
+      <Typography
+        component="span"
+        className={`${classes.text} ${classes.border}`}
+      >
+        Add Review
+      </Typography>
+      <Typography component="span" className={classes.text}>
+        Add Request
+      </Typography>
     </Button>
   );
 }
