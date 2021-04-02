9a26de07884943fe825371cfebdf033afcbbfc9b
rudietuesdays <rae@throneless.tech>
2021-04-02 17:14:35 -0400

    building out barplot MUI port

--- src/frontend/components/barplot.js
+++ src/frontend/components/barplot.js
@@ -3,10 +3,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 // Material UI imports
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
+import ListItemMui from '@material-ui/core/ListItem';
 import Typography from '@material-ui/core/Typography';
+
+const ListItem = withStyles(theme => ({
+  root: {
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+  },
+}))(ListItemMui);
 
 const useStyles = makeStyles(theme => ({
   title: {
@@ -14,9 +23,11 @@ const useStyles = makeStyles(theme => ({
     textTransform: 'uppercase',
   },
   key: {
+    color: theme.palette.secondary.main,
     paddingLeft: 20,
     paddingRight: 20,
     position: 'relative',
+    textTransform: 'uppercase',
     '&:before': {
       content: '""',
       height: 15,
@@ -46,6 +57,26 @@ const useStyles = makeStyles(theme => ({
     '&:before': {
       backgroundColor: theme.palette.reviews.main,
     },
+  },
+  response: {
+    height: 22,
+  },
+  responseText: {
+    color: theme.palette.primary.contrastText,
+    paddingBottom: 4,
+    paddingLeft: 4,
+  },
+  responseNa: {
+    backgroundColor: '#000',
+  },
+  responseNo: {
+    backgroundColor: theme.palette.primary.light,
+  },
+  responseUnsure: {
+    backgroundColor: theme.palette.secondary.main,
+  },
+  responseYes: {
+    backgroundColor: theme.palette.reviews.main,
   },
 }));
 
@@ -97,9 +128,57 @@ export default function Barplot({ stats, nReviews, children }) {
       <List>
         {stats ? (
           stats.map(({ questionId, nReviews, question, yes, no, na, unsure }) => (
-            <ListItem key={questionId}>
-            </ListItem>
-          ))
+              <ListItem key={questionId}>
+                <Grid container alignItems="center" spacing={2}>
+                  <Grid item xs={12} sm={6}>
+                    <Typography component="div" variant="body1">
+                      {question}
+                    </Typography>
+                  </Grid>
+                  <Grid container item spacing={0} xs={12} sm={6}>
+                    <Grid
+                      item
+                      className={`${classes.response} ${classes.responseYes}`}
+                      style={{ width: `${(yes.length / nReviews) * 100}%` }}
+                    >
+                      <Typography className={classes.responseText}>
+                        {`${(yes.length / nReviews) * 100}%`}
+                      </Typography>
+                    </Grid>
+                    <Grid
+                      item
+                      className={`${classes.response} ${
+                        classes.responseUnsure
+                      }`}
+                      style={{ width: `${(unsure.length / nReviews) * 100}%` }}
+                    >
+                      <Typography className={classes.responseText}>
+                        {`${(unsure.length / nReviews) * 100}%`}
+                      </Typography>
+                    </Grid>
+                    <Grid
+                      item
+                      className={`${classes.response} ${classes.responseNa}`}
+                      style={{ width: `${(na.length / nReviews) * 100}%` }}
+                    >
+                      <Typography className={classes.responseText}>
+                        {`${(na.length / nReviews) * 100}%`}
+                      </Typography>
+                    </Grid>
+                    <Grid
+                      item
+                      className={`${classes.response} ${classes.responseNo}`}
+                      style={{ width: `${(no.length / nReviews) * 100}%` }}
+                    >
+                      <Typography className={classes.responseText}>
+                        {`${(no.length / nReviews) * 100}%`}
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                </Grid>
+              </ListItem>
+            ),
+          )
         ) : (
           <Typography component="div" variant="body2">
             No rapid reviews. Would you like to leave one?
