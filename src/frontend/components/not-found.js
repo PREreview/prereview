b2060bdc963f77a3f727ed0914214c3ed3b36261
rudietuesdays <rae@throneless.tech>
2021-04-05 17:24:52 -0400

    port not found component to MUI

--- src/frontend/components/not-found.js
+++ src/frontend/components/not-found.js
@@ -5,10 +5,14 @@ import { Helmet } from 'react-helmet-async';
 // contexts
 import UserProvider from '../contexts/user-context';
 
+// MaterialUI imports
+import Grid from '@material-ui/core/Grid';
+import Link from '@material-ui/core/Link';
+import Typography from '@material-ui/core/Typography';
+
 // components
 import HeaderBar from './header-bar';
 import Org from './org';
-import XLink from './xlink';
 
 // constants
 import { ORG } from '../constants';
@@ -17,23 +21,33 @@ export default function NotFound() {
   const [thisUser] = useContext(UserProvider.context);
 
   return (
-    <div className="not-found">
+    <>
       <HeaderBar thisUser={thisUser} />
 
       <Helmet>
         <title>{ORG} • Not Found</title>
       </Helmet>
 
-      <div className="not-found__body">
-        <h1>Not found</h1>
-
-        <p>
-          Visit <Org />{' '}
-          <XLink to="/" href="/">
-            Homepage
-          </XLink>
-        </p>
-      </div>
-    </div>
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justify="center"
+        style={{ minHeight: '80vh' }}
+      >
+        <Grid item xs={3}>
+          <Typography component="h2" variant="h2">
+            Not found
+          </Typography>
+          <Typography component="div" variant="body2">
+            Visit <Org />{' '}
+            <Link href="/" color="primary">
+              homepage
+            </Link>
+          </Typography>
+        </Grid>
+      </Grid>
+    </>
   );
 }
