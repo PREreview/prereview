a86ac432023eff3c98e190ff43c28925cfbcc615
Harum Helmy <harumendhah@gmail.com>
2021-04-06 09:57:19 -0400

    Added conditional to check if theres user logged in

--- src/frontend/utils/roles.js
+++ src/frontend/utils/roles.js
@@ -1,4 +1,4 @@
 export function checkIfProfileNeedsUpdate(user) {
   // returns true if the user doesn't have any contacts in their profile or their persona doesn't have an avatar
-  return !(user.contacts.length > 0) || !user.defaultPersona.avatar;
+  return user && user.contacts ? !(user.contacts.length > 0) || !user.defaultPersona.avatar : false
 }
