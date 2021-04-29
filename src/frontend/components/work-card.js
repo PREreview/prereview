ddea6e8785c48dbaf1708700c4865e8da5f1d747
Harum Helmy <harumendhah@gmail.com>
2021-04-28 22:14:53 -0400

    Fix broken build

--- src/frontend/components/work-card.js
+++ src/frontend/components/work-card.js
@@ -6,6 +6,7 @@ import { format } from 'date-fns';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
+import Grid from '@material-ui/core/Grid';
 import Link from '@material-ui/core/Link';
 import Typography from '@material-ui/core/Typography';
 
