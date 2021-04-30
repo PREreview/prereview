91632af4c8fe54bc7dfec842238071c4c6e6e272
rudietuesdays <rae@throneless.tech>
2021-04-30 15:43:32 -0400

    #304 add links to preprint on server in preprint preview

--- src/frontend/components/preprint-preview.js
+++ src/frontend/components/preprint-preview.js
@@ -1,6 +1,7 @@
 // base imports
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { useHistory } from 'react-router-dom';
 
 // utils
 import { getFormattedDatePosted } from '../utils/preprints';
@@ -98,6 +99,10 @@ const useStyles = makeStyles(theme => ({
 
 export default function PreprintPreview({ preprint }) {
   const classes = useStyles();
+  const history = useHistory();
+
+  console.log(preprint);
+
   const [elevation, setElevation] = useState(0);
   const preprintId = createPreprintId(preprint.handle);
   const publishedReviews = preprint.fullReviews.filter(
@@ -135,7 +140,7 @@ export default function PreprintPreview({ preprint }) {
         </Grid>
         <Grid item xs={12} sm={8}>
           <Typography>
-            <Link href={`/preprints/${preprintId}`} className={classes.title}>
+            <Link href={preprint.url} className={classes.title}>
               {preprint.title}
             </Link>
           </Typography>
@@ -144,7 +149,9 @@ export default function PreprintPreview({ preprint }) {
               {preprint.preprintServer}
               <ChevronRightIcon className={classes.icon} />
             </span>
-            <span className={classes.meta}>{preprint.handle}</span>
+            <Link href={preprint.url} className={classes.meta}>
+              {preprint.handle}
+            </Link>
           </Typography>
         </Grid>
       </Grid>
