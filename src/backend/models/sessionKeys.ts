aa6aafa4b3c20ac35f42c204e776c64b42e6a7c2
Chris Wilkinson <c.wilkinson@elifesciences.org>
2021-08-16 13:43:08 +0100

    refactor: remove unused backend modules
    Refs #397

--- src/backend/models/sessionKeys.ts
+++
@@ -1,9 +0,0 @@
-import { EntityRepository, MikroORM, Repository } from '@mikro-orm/core';
-import { SessionKey } from './entities';
-
-@Repository(SessionKey)
-export class SessionKeyModel extends EntityRepository<SessionKey> {}
-
-export function sessionKeyModelWrapper(db: MikroORM): SessionKeyModel {
-  return db.em.getRepository(SessionKey);
-}
