5b1c1139c496668923c7a82ac7afee861f86b8d5
Chris Wilkinson <c.wilkinson@elifesciences.org>
2021-08-16 11:42:25 +0100

    refactor: remove unused components
    There's a whole directory full of 'archived' components. Since they are unused, they should be removed. (Since the code is under version control, that's a more suitable archive.)
    
    Refs #397

--- src/frontend/components/archived/notice-badge.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const NoticeBadge = () => <div className="notice-badge">!</div>;
-
-export default NoticeBadge;
--- src/frontend/components/archived/org.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function Org() {
-  return <span>PREreview.org</span>;
-}
