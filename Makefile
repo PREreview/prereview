PREreview/prereview

commit e21dc0c2358d23593ec43746e23115375116935c

build(docker): mount an empty folder for the integration test results
GitHub Actions has seen some failures due to permission problems. This is caused by the mounted folder not existing, so Docker has to create it. On Linux, the permissions are based on the container, which causes a failure when the host tries to subsequently write to it. (This doesn't happen on Macs due to the Docker For Mac intermediary virtual machine.)

So, this makes sure the folder exists but is empty before mounting.

Refs #388

Makefile
@@ -55,7 +55,7 @@ test: build ## Run the tests
 integration-test: export TARGET = integration
 integration-test: export LOG_FILE = integration/results/docker.log
 integration-test: build ## Run the integration tests
-	rm -rf integration/results
+	mkdir -p integration/results && rm -r integration/results/*
 	${DOCKER_COMPOSE} run --rm playwright; ${LOGS}
 
 smoke-test: build ## Run the smoke tests

commit 08174856bde98c8c13efe3cf1c52380c3dc18c94

build(make): fix removal of files in an empty directory
e21dc0c broke when the directory was empty; this should now do nothing in those cases.

Refs #388

Makefile
@@ -55,7 +55,7 @@ test: build ## Run the tests
 integration-test: export TARGET = integration
 integration-test: export LOG_FILE = integration/results/docker.log
 integration-test: build ## Run the integration tests
-	mkdir -p integration/results && rm -r integration/results/*
+	mkdir -p integration/results && rm -rf integration/results/*
 	${DOCKER_COMPOSE} run --rm playwright; ${LOGS}
 
 smoke-test: build ## Run the smoke tests