391c2dacbdf4eedf51d01b73811862136214158f
Chris Wilkinson <c.wilkinson@elifesciences.org>
2021-09-08 10:35:14 +0100

    fix: add type
    The use of 'unknown' is better than 'any' because it's at least type-safe, but this should set the correct type.
    
    Note this doesn't fix all the typing problems in this code.
    
    Refs #398

--- src/backend/utils/getFields.ts
+++ src/backend/utils/getFields.ts
@@ -1,34 +1,40 @@
 import 'reflect-metadata';
+import { EntityProperty, FindOptions } from '@mikro-orm/core';
 import * as entities from '../models/entities';
 
-export function getFields(
-  type: keyof typeof entities,
+type Fields<T> = NonNullable<FindOptions<T>['fields']>;
+
+export function getFields<T extends keyof typeof entities>(
+  type: T,
   includes?: string[],
   recurse = 2,
-): unknown {
-  return Object.values(entities[type].prototype.__meta.properties).reduce(
-    (props: any[], prop: any) => {
-      if (!(prop.hidden === true)) {
-        if (prop.reference === 'scalar') {
-          if (
-            prop.type !== 'BlobType' ||
-            (includes && includes.includes(prop.name))
-          ) {
-            props.push(prop.name);
-          }
-          return props;
+): Fields<T> {
+  return Object.values(entities[type].prototype.__meta.properties).reduce<
+    Fields<T>
+  >((props, prop: EntityProperty<typeof entities[T]>) => {
+    if (!(prop.hidden === true)) {
+      if (prop.reference === 'scalar') {
+        if (
+          prop.type !== 'BlobType' ||
+          (includes && includes.includes(prop.name))
+        ) {
+          props.push(prop.name);
         }
-        if (recurse > 0) {
-          if (prop.reference === 'm:1') {
-            props.push(prop.name);
-          }
-          props.push({
-            [prop.name]: getFields(prop.type, includes, recurse - 1),
-          });
-        }
+        return props;
       }
-      return props;
-    },
-    [],
-  );
+      if (recurse > 0) {
+        if (prop.reference === 'm:1') {
+          props.push(prop.name);
+        }
+        props.push({
+          [prop.name]: getFields(
+            prop.type as keyof typeof entities,
+            includes,
+            recurse - 1,
+          ),
+        });
+      }
+    }
+    return props;
+  }, []);
 }
