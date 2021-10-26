4cb6f2221606eb52db8adac1e47a4159039f7f2e
Chris Wilkinson <c.wilkinson@elifesciences.org>
2021-10-26 15:50:29 +0100

    test(jest) allow tests to run for longer
    I can't work out why Jest is timing out in CI; I'm not able to reproduce it locally. To see if it really is just being slow, this increases the timeout for a test to be 30 seconds rather than 5.
    
    It also disables ts-jest's diagnostics, which should improve performance slightly too.
    
    Refs ba79ea2, #388

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,12 @@
 const config = {
   globals: {
     'ts-jest': {
+      diagnostics: false,
       isolatedModules: true,
     },
   },
   preset: 'ts-jest/presets/js-with-ts',
+  testTimeout: 30 * 1000,
   verbose: true,
 };
 
